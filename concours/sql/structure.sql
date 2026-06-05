564ab44ed01da75c423d82685ec6edafa566f3bc
Antoine Menel <Antoine78560@gmail.com>
2026-06-05 11:02:23 +0200

    10 questions

--- concours/sql/structure.sql
+++ concours/sql/structure.sql
@@ -8,3 +8,15 @@ CREATE TABLE question (
     reponse BOOLEAN NOT NULL,
     points INT NOT NULL
 );
+
+INSERT INTO question (texte, reponse, points) VALUES
+('Paris est la capitale de la France.', 1, 10),
+('Une semaine contient 7 jours.', 1, 20),
+('Les araignees sont des insectes.', 0, 30),
+('L''eau bout a 100°C au niveau de la mer.', 1, 40),
+('La Terre possede deux lunes naturelles.', 0, 50),
+('Le langage PHP s''execute principalement cote serveur.', 1, 60),
+('Le nombre PI est un nombre rationnel.', 0, 70),
+('Un octet est compose de 8 bits.', 1, 80),
+('La vitesse de la lumiere dans le vide est superieure a 300000 km/s.', 0, 90),
+('En SQL, une cle etrangere garantit l''integrite referentielle entre deux tables.', 1, 100);
