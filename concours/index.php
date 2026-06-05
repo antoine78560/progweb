da1d5fe73ec705f8c9775c1630f7e32a5f4584e8
Antoine Menel <Antoine78560@gmail.com>
2026-06-05 11:15:24 +0200

    index php

---
+++ concours/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Concours Vrai ou Faux</title>
+</head>
+<body>
+
+    <h1>Concours Vrai ou Faux</h1>
+
+    <p>Bienvenue sur notre jeu de questions.</p>
+
+</body>
+</html>
